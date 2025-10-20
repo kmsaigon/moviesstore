49d983cd1987f0dde4049b301a047a9238a38650
vivianhd6 <vdiep2006@gmail.com>
2025-10-19 20:35:24 -0400

    Update views.py

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Movie, Review, MovieRequest
+from .models import Movie, Review, MovieRequest, Rating
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST
 from django.core.paginator import Paginator
@@ -20,12 +20,33 @@ def index(request):
                   {'template_data': template_data})
 
 def show(request, id):
-    movie = Movie.objects.get(id=id)
-    reviews = Review.objects.filter(movie=movie)
+    movie = get_object_or_404(Movie, id=id)
+    reviews = Review.objects.filter(movie=movie).order_by('-date')
+
+    user_rating = None
+    if request.user.is_authenticated:
+        try:
+            user_rating = Rating.objects.get(movie=movie, user=request.user)
+        except Rating.DoesNotExist:
+            pass
+
+    if request.method == 'POST' and 'rating' in request.POST and request.user.is_authenticated:
+        rating_value = request.POST.get('rating')
+        if rating_value:
+            rating, created = Rating.objects.update_or_create(
+                movie=movie,
+                user=request.user,
+                defaults={'rating': rating_value}
+            )
+            return redirect('movies.show', id=id)
+
     template_data = {}
     template_data['title'] = movie.name
     template_data['movie'] = movie
     template_data['reviews'] = reviews
+    template_data['user_rating'] = user_rating
+    template_data['average_rating'] = movie.average_rating()
+
     return render(request, 'movies/show.html',
                   {'template_data': template_data})
 
